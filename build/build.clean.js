30dea2b83718bd48bb69358c3eb1830dd52a4a72
eswat109 <eswat98@mail.ru>
2020-02-01 05:14:38 +1000

    add grunt

--- build/build.clean.js
+++
@@ -1,3 +0,0 @@
-/* node - v1.0.0 - 2020-02-01 */
-
-$("#formBtn").click(function(){var r=$("#orderForm").serialize();$.ajax({type:"post",url:"/order",data:r,success:function(r){$("#form").hide(),$("body").append(r)}})});
